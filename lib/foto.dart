3f2672a260bcd8e3a9e9a3f82b64d7213e66899b
daniertg <febriandani00@gmail.com>
2024-11-26 15:41:28 +0700

    first commit

---
+++ lib/foto.dart
@@ -0,0 +1,15 @@
+export 'package:camera_platform_interface/camera_platform_interface.dart'
+    show
+        CameraDescription,
+        CameraException,
+        CameraLensDirection,
+        ExposureMode,
+        FlashMode,
+        FocusMode,
+        ImageFormatGroup,
+        ResolutionPreset,
+        XFile;
+
+export 'src/foto_controller.dart';
+export 'src/foto_image.dart';
+export 'src/foto_preview.dart';
